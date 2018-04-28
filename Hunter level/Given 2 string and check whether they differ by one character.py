b2dd061277ec6aed0baecb6661455b6d6bfd40f6
15BEC078 <37336213+15BEC078@users.noreply.github.com>
2018-04-28 17:46:25 +0530

    Create Given 2 string and check whether they differ by one character.py

---
+++ Hunter level/Given 2 string and check whether they differ by one character.py
@@ -0,0 +1,15 @@
+a=str(input())
+b=str(input())
+n1=len(a)
+n2=len(b)
+count=0
+if(n1==n2):
+  for i in range(n1):
+    if(a[i]==b[i]):
+      count=count+0
+    else:
+      count=count+1 
+if(count==1):
+  print("yes")
+else:
+  print("no")
